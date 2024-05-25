92fbba7014e6cd1cede0a9aea3d7431fbc42ca79
Obadiah Onikoyi <obaonikoyi@gmail.com>
2024-05-25 19:27:32 +0930

    Add program and head office to home page

---
+++ jsscript.js
@@ -0,0 +1,33 @@
+//jsscript.js
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+const images = [
+    'images/headoffice.jpg',
+    'images/headoffice2.jpg',
+    'images/headoffice3.jpg',
+    'images/headoffice4.jpg',
+    'images/headoffice5.jpg'
+];
+
+let currentImageIndex = 0;
+const imageElement = document.getElementById('headOfficeImage');
+
+function changeImage() {
+    imageElement.style.opacity = 0; // Start fade-out
+    setTimeout(() => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        imageElement.src = images[currentImageIndex];
+        imageElement.style.opacity = 1; // Start fade-in
+    }, 1000); // Duration of fade-out (1 second)
+}
+
+// Change image every 3 seconds
+setInterval(changeImage, 3000);
